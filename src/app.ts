650e757f198bd2eeae1fdc60b6b2fc27294c75bf
Dhyan Rai <172551908+raaid3@users.noreply.github.com>
2025-06-04 02:42:23 -0400

    Add CORS support

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import { apiRouter } from "./routes/apiRoute.js";
+import cors from "cors";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 app.use(express.json());
+app.use(cors());
 app.use("/api", apiRouter);
 
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT} and host 0.0.0.0`);
+  console.log(`Server listening on port ${PORT}`);
 });
